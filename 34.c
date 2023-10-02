020ce417436c119ef389b0709a5497725903a3ac
Ayushmaan <kapruwanayush3008@outlook.com>
2023-10-02 20:16:07 +0530

    added some files

---
+++ 34.c
@@ -0,0 +1,33 @@
+//Write a C Program to Find the Grade of a Student Using Switch Case
+
+#include <stdio.h>
+int main() {
+    int score;
+    char grade;
+
+    printf("Enter the student's score: ");
+    scanf("%d", &score);
+
+    switch(score / 10) {
+        case 10:
+        case 9:
+            grade = 'A';
+            break;
+        case 8:
+            grade = 'B';
+            break;
+        case 7:
+            grade = 'C';
+            break;
+        case 6:
+            grade = 'D';
+            break;
+        default:
+            grade = 'F'; 
+            break;
+    }
+
+    printf("The student's grade is: %c\n", grade);
+
+    return 0;
+}
